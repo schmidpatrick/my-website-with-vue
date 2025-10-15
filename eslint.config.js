a9a3a7ef45c1ec14cd924db782a7325d4ee67ea3
Patrick <schmidpatrick@outlook.com>
2025-10-15 16:00:12 +0200

    Fix linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,41 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
+import eslintPluginVue from "eslint-plugin-vue";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
 
 export default [
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    files: ["**/*.ts", "**/*.vue"],
+    ignores: ["dist", "node_modules"],
+
+    languageOptions: {
+      parser: require("vue-eslint-parser"), // Vue parses first ...
+      parserOptions: {
+        parser: tsParser, // ... then TS for <script lang="ts">
+        ecmaVersion: 2020,
+        sourceType: "module",
+        extraFileExtensions: [".vue"],
+      },
+    },
+
+    plugins: {
+      vue: eslintPluginVue,
+      "@typescript-eslint": tseslint,
+    },
+
+    rules: {
+      ...eslintPluginVue.configs["vue3-recommended"].rules,
+      ...tseslint.configs.recommended.rules,
+
+      // 👇 Optional relaxations / tuning
+      "vue/multi-word-component-names": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
   },
 
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
-
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-]
+  prettierConfig,
+];
